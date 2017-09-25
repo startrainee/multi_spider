d61d6fd5455dbb4f3cf5106b8372503e36d926aa
suhuanzhen <1334088767@qq.com>
2017-09-25 10:19:10 +0800

    add a class URLOutputer; add methods implements.

--- src/com/startrainee/spider/HTMLOutputer.java
+++ src/com/startrainee/spider/HTMLOutputer.java
@@ -1,10 +1,19 @@
 package com.startrainee.spider;
 
+import java.io.File;
 import java.util.List;
 
 public class HTMLOutputer {
-
+    private static int fileCount = 0;
+    private static String baseFileName = "spider_data_";
     public void outPut(List<String> parseredData) {
-
+        String fileName = baseFileName + fileCount +".txt";
+        File file = new File(fileName);
+        while (file.exists()){
+            fileCount++;
+            fileName = baseFileName + fileCount;
+            file = new File(fileName);
+        }
+        file.mkdirs();
     }
 }
